2c07106ca34275266777eb3c729d8c3e5e53d9e6
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-12-05 19:26:02 -0700

    changed format but still isnt working

--- server.js
+++ server.js
@@ -1,15 +1,63 @@
 const express = require("express");
-const apiRoute = require("./Routes/apiRoute");
-
-// Initialize the app and create a port
-const app = express();
+const db = require("./db/db.json");
+const fs = require("fs");
+const path = require("path");
+//adds unique identifyer using NPM UUID
+const uuid = require("./helper/uuid");
 const PORT = process.env.PORT || 3001;
 
-// Middleware
-app.use(express.json());
+const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//adds static HTML CSS and JS
 app.use(express.static("public"));
-app.use(`/api`, apiRoute);
 
-// Listen on PORT
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+app.get("/notes", (req, res) => {
+  data = path.join(__dirname, "./public/notes.html");
+  //pulls second HTML page and renders
+  res.sendFile(data);
+});
+
+app
+  .route("/api/notes")
+  .get((req, res) => {
+    //pulls the added notes from memory
+    console.info(`${req.method} requested`);
+    res.send(JSON.parse(fs.readFileSync("./db/db.json", "utf8")));
+  })
+  .post((req, res) => {
+    console.info(`${req.method} Post`);
+    const madeNote = req.body;
+    //gets unique identifier
+
+    madeNote.id = uuid();
+
+    //turns string into object and preps file to be added to
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    //add new object to data
+    data.push(madeNote);
+
+    //stringify before sending to json file
+    fs.writeFileSync("./db/db.json", JSON.stringify(data));
+    res.status(201).json(data);
+  });
+app.delete(`/api/notes/:id`, (req, res) => {
+  let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  //goes through all objects to see which matches the passed in ID
+  const indexOfObj = data.findIndex((obj) => {
+    return obj.id === req.params.id;
+  });
+  //pull out deleted element of the object
+  data.splice(indexOfObj, 1);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(data));
+
+  res.send("data has been deleted.");
+});
+
+//wild card route for not listed URL entries
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
